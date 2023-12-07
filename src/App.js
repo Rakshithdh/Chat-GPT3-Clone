5a455a4c88703097617b4fd2fe5e0c0c9c2f0b7b
rakshith <rakshith.durai@gmail.com>
2023-12-08 00:32:33 +0530

    Remove Article and Blog components from exports

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { CTA, Brand, Navbar } from './components'
 import './App.css'
 
 const App = () => {
-  return (
+  return (  
     <div className='App'>
       <div className='gradient__bg'>
         <Navbar />
